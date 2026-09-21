f279cf56fc6c6b4a8368c1e1ba2afc2a6d541cd6
traitay95 <traitay95@gmail.com>
2026-09-21 19:47:44 +0700

    Update lich-tuan.py

--- lich-tuan.py
+++ lich-tuan.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title="Hệ Thống Lịch Phòng Kế Hoạch", layout=
 # ---------------------------------------------------------
 # Dán URL và ANON_KEY từ Supabase vào đây (hoặc cấu hình trong st.secrets)
 SUPABASE_URL = st.secrets.get("SUPABASE_URL", "https://rsxjvquijelfylkevgwt.supabase.co")
-SUPABASE_KEY = st.secrets.get("SUPABASE_KEY", "sb_publishable_SWPd-hu2i253AcIc31nqyg_UJsc7Y0C")
+SUPABASE_KEY = st.secrets.get("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzeGp2cXVpamVsZnlsa2V2Z3d0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5ODUwMjUsImV4cCI6MjEwNTU2MTAyNX0.L6900iNgKbaLAaU17RgdGpD_ccuXUqJjsEF3KF6jivs")
 
 SENDER_EMAIL = "traitay95@gmail.com"
 SENDER_PASSWORD = "wtgm paga vpze bfzm"
